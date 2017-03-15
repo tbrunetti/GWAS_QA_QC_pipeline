67dd6fe0231d63f741ed0c5d0c4931947562e9e2
tbrunetti <tonya.brunetti@colorado.edu>
2017-03-15 11:31:12 -0600

    added PDF save feature

--- basic_statistics.py
+++ basic_statistics.py
@@ -5,6 +5,7 @@ import matplotlib.collections as collections
 import seaborn as sns
 import pandas
 import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
 
 def basic_stats(programLoc, inputFile, outputPrefix):
 	filename = args.inputFile.rsplit('/', 1)[-1]
@@ -16,6 +17,7 @@ def basic_stats(programLoc, inputFile, outputPrefix):
 		print("File is not a .ped or .bed, please convert")
 
 def visualize_stats(outputPrefix, custom_file):
+	
 	# read in custom_file if user supplies one
 	if custom_file != None:
 		custom_snp_dict = {}
@@ -25,15 +27,18 @@ def visualize_stats(outputPrefix, custom_file):
 				custom_snp_dict[snp] = [chrm, pos]
 
 		custom_names_only = [key for key in custom_snp_dict]
-
+	
+	
 	def maf_analysis():
 		# distribution of SNP counts by MAF
 		freq_file = pandas.read_table(outputPrefix + '.frq', delim_whitespace=True)
-		maf_dist = freq_file['MAF'].plot.hist()
+		plt.figure()
+		maf_dist = freq_file['MAF'].plot.hist(bins=10)
 		plt.xlabel("MAF scores")
 		plt.ylabel("Total Numer of SNPs")
 		plt.title("Distribution of MAF across all SNPs", fontsize=15)
-		plt.show()
+		pdf.savefig()
+		plt.close()
 
 		# graphing of MAF information by number SNPs retained at each threshold; answers the question "How many SNPs have a given MAF score within each MAF interval"
 		MAF_iterables = [0.0, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.10, 0.20]
@@ -63,21 +68,41 @@ def visualize_stats(outputPrefix, custom_file):
 			# graph percentage retained between custom and all snps in chip		
 			percentage_maf_comparison = pandas.DataFrame(np.matrix(custom_temp_array+all_temp_arrary), columns=['MAF_threshold', 'content', 'percent_retained'])
 			percentage_maf_comparison[['percent_retained']] = percentage_maf_comparison[['percent_retained']].astype(float)
+			plt.figure()
 			maf_comparison = sns.barplot(x='MAF_threshold', y='percent_retained', hue='content', data=percentage_maf_comparison)
 			maf_comparison.set(xlabel='MAF threshold filtered', ylabel='percentrage of SNPs retained')
-			sns.plt.show()	
+			sns.plt.title('Total percentage of SNPs remaining at each MAF threshold')
+			pdf.savefig()
+			plt.close()
 		
 
 		# no custom file required here; answers the question "How many SNPs remain at each MAF threshold after filtering by MAF"	
-		plt.bar(np.arange(len(str_MAF_iterables)), total_snps)
+		plt.figure()
+		plt.bar(np.arange(len(str_MAF_iterables)), total_snps, align='center')
 		plt.xticks(np.arange(len(str_MAF_iterables)), str_MAF_iterables)
 		plt.title('Total Number of SNPs Retained by MAF threshold', fontsize=20)
 		plt.xlabel('MAF cut-off', fontsize=15)
 		plt.ylabel('Total number of SNPs retained', fontsize=15)
-		plt.show()
+		pdf.savefig()
+		plt.close()
 
 
-	maf_analysis()
+	def hwe():
+		# distribution of SNP counts by MAF
+		hwe_file = pandas.read_table(outputPrefix + '.hwe', delim_whitespace=True)
+		print hwe
+
+	def missing():
+		pass;
+
+	def mendel():
+		pass;
+
+
+	hwe()
+	#with PdfPages(str(outputPrefix)+'-basic-analysis.pdf') as pdf:
+	#	maf_analysis()
+
 
 
 
